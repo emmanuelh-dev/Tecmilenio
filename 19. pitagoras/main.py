78c63709f4062d12ba552e8bdf5fa2c4d3b4033c
Emmanuel Hernandez <e805177@gmail.com>
2024-09-02 20:02:31 -0700

    pitagoras

---
+++ 19. pitagoras/main.py
@@ -0,0 +1,13 @@
+# Realizarás un programa en Python para mostrar la tabla de Pitágoras, la cual permite visualizar cuál será el resultado del producto de dos números,
+# así que solicitarás dos factores (cifras) para obtener el resultado de su multiplicación.
+def pitagoras():
+  num1 = int(input("Ingresa el primer numero"))
+  num2 = int(input("Ingresa el segundo numero"))
+
+  print("Tabla de pitagoras")
+  for i in range(num1):
+    for j in range(num2):
+      print(f"{i * j:4}", end=" ")
+    print()
+  
+pitagoras()
